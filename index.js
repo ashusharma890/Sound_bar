5e0d02368ed605e448f3da8a955c580151ee25b2
ashusharma890 <81765508+ashusharma890@users.noreply.github.com>
2022-07-22 02:29:25 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const btn =document.querySelectorAll(".btn");
+
+const stopSounds = () => {
+    btn.forEach((button) => {
+      const sound = document.getElementById(button.innerText);
+      sound.pause();
+        sound.currentTime=0;
+    });
+};
+
+btn.forEach((button) => {
+    button.addEventListener("click", () => {
+      stopSounds();
+      console.log(document.getElementById(button.innerText));
+      document.getElementById(button.innerText).play();
+    });
+  });
+
+// const btn = document.querySelectorAll(".btn");
+
+// const stopSounds = () => {
+//   btn.forEach((button) => {
+//     const sound = document.getElementById(button.innerText);
+//     sound.pause();
+//     sound.currentTime = 0;
+//   });
+// };
+
+// btn.forEach((button) => {
+//   button.addEventListener("click", () => {
+//     stopSounds();
+//     document.getElementById(button.innerText).play();
+//   });
+// });
